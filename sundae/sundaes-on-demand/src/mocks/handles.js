68cea6b7f5ebabe529bcf9d3c8216d7d656b9132
cristian.orozco <cristian.orozco@globant.com>
2022-08-17 17:16:33 -0500

    Mock Service Worker configuration

---
+++ sundae/sundaes-on-demand/src/mocks/handles.js
@@ -0,0 +1,12 @@
+import { res } from 'msw'
+
+export const handlers = [
+  res.get('http://localhost:3030/scoops', (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: 'Chocolate', imagePath: '/images/chocolate.png' },
+        { name: 'Vanilla', imagePath: '/images/vanilla.png' },
+      ])
+    )
+  }),
+]
